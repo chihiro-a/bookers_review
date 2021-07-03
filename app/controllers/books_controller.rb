dc19e9a1b841fa3e83a6a0df683c0e2357a23cf1
chihiro-a <chihiro.i.0128@gmail.com>
2021-07-03 14:47:54 +0000

    [update]booksのルーティングを修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,18 @@
 class BooksController < ApplicationController
+    def new
+        @book = Book.new
+    end
+    
+    def index
+        @books = Book.all
+    end
+    
+    def edit
+        @book = Book.find(params[:id])
+        redirect_to book_path(@book)
+    end
+    def destroy
+        @book = Book.find(params[:id])
+        redirect_to books_path
+    end
 end
