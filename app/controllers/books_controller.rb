cf27fb08ce5fd1b407c5ae595682844247b6b0b8
chihiro-a <chihiro.i.0128@gmail.com>
2021-07-03 15:13:58 +0000

    [create]投稿機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,11 @@
 class BooksController < ApplicationController
-    def new
-        @book = Book.new
-    end
-    
     def create
         @book = Book.new(book_params)
+        redirect_to books_path
     end
     
     def index
+        @book = Book.new
         @books = Book.all
     end
     
