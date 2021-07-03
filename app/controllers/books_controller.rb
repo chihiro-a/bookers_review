a987853f0d8ab6371116ed1191f4b5f59aafc801
chihiro-a <chihiro.i.0128@gmail.com>
2021-07-03 14:53:54 +0000

    [create]booksコントローラを記述

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,16 +3,32 @@ class BooksController < ApplicationController
         @book = Book.new
     end
     
+    def create
+        @book = Book.new(book_params)
+    end
     def index
         @books = Book.all
     end
     
+    def show
+        @book = Book.find(params[:id])
+    end
     def edit
         @book = Book.find(params[:id])
-        redirect_to book_path(@book)
+    end
+    
+    def update
+        @book = Book.find(params[:id])
+        @book.update(book_params)
+        redirect_to book_path
     end
     def destroy
         @book = Book.find(params[:id])
         redirect_to books_path
+    end
+    
+    private
+    def book_params
+        params.require(:book).permit(:title, :body)
     end
 end
